6aaa321f5d358f5c4f83804cdc1aa4d2fb2b0e12
Diego Gaibor <diego.g@scopicsoftware.com>
2025-12-15 19:27:34 -0500

    Register page

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,34 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import { RegisterPage } from '@/pages/register/Register.tsx';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+               path: "login",
+                element: <div>Login Page</div>,
+            },
+            {
+                path: "register",
+                element: <RegisterPage/>,
+            },
+            {
+                path: "shop",
+                element: <div>Shop Page</div>,
+            }
+        ]
+            
+    },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </StrictMode>,
 )
